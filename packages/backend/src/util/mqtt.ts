f63756e05b7f34e51aeb58370fcf11517e2e86aa
Edwin Martin <edwin@bitstorm.org>
2025-08-01 22:55:02 +0200

    feature(stats): improve sending stats to mqtt

--- packages/backend/src/util/mqtt.ts
+++ packages/backend/src/util/mqtt.ts
@@ -3,6 +3,12 @@ import { getAndAssertEnv } from "@shared/config/sharedConfig";
 import mqtt from "mqtt";
 
 export async function startMqtt(badgeHubData: BadgeHubData) {
+  const nodeEnv = getAndAssertEnv("NODE_ENV");
+
+  if (nodeEnv=='development') {
+    console.log('Not publishing to MQTT in development environment');
+    return;
+  }
 
   const username = getAndAssertEnv("MQTT_USER");
   const password = getAndAssertEnv("MQTT_PASSWD");
@@ -10,14 +16,27 @@ export async function startMqtt(badgeHubData: BadgeHubData) {
   const server = getAndAssertEnv("MQTT_SERVER");
   const interval = getAndAssertEnv("MQTT_INTERVAL_SEC");
 
+  if (!server) {
+    console.log('MQTT_SERVER not set');
+    return;
+  }
 
   let client = mqtt.connect(server, {
-    username,password
+    username,
+    password,
+    clean: true
   });
 
-  setInterval(async () => {
-    const stats = await badgeHubData.getStats();
+  client.on('connect', () => {
+    console.log('Connected to MQTT server');
+    setInterval(async () => {
+      const stats = await badgeHubData.getStats();
 
-    client.publish(topic, JSON.stringify(stats))
-  }, 10_000)
+      client.publish(
+        topic,
+        JSON.stringify({ ...stats, timestamp: new Date().toISOString() }),
+        { qos: 1, retain: true }
+      );
+    }, Number(interval));
+  });
 }
